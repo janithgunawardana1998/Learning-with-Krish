235b50773574d1c6004bb253a74cea27dc9dd89b
janithgunawardana1998 <janithgunawardana98@gmail.com>
2021-12-31 18:57:20 +0530

    First Commit

---
+++ Static Block/St.java
@@ -0,0 +1,19 @@
+Class Learning_With_Krish_HW_2
+{
+
+{
+public static void main(String[] args) {
+
+ A a =new A ();
+}
+
+{
+ System.out.println("This is Empty Bloack")
+
+}
+static
+{
+System.out.println("This is Static Bloack")
+
+}
+}
---
+++ Static Block/StaticApp.java
@@ -0,0 +1,24 @@
+class StaticApp
+{
+public static void main(String[] args) {
+
+ StaticApp a =new StaticApp ();
+}
+
+{
+ System.out.println("This is Empty Bloack");
+
+}
+static
+{
+System.out.println("This is Static Bloack");
+
+}
+public StaticApp ()
+{
+
+System.out.println("This will be the constructor");
+
+}
+
+}
